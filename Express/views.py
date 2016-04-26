52b1ee2c79189596c93a3d88ad861f4115f934e6
ryh <1971990184@qq.com>
2016-04-26 10:22:41 +0800

    add parameters

--- Express/views.py
+++ Express/views.py
@@ -13,20 +13,28 @@ from django.shortcuts import render, render_to_response
 def index(request):
     # 1.get info from the request
     if request.method == 'GET':
-        name =  request.GET['name']
-        address = request.GET['address']
-        phone = request.GET['phone']
-    # 2.create Qrcode
+        myName =  request.GET['myName']
+        myPhone = request.GET['myPhone']
+        myAddress = request.GET['myAddress']
+        myPostcode = request.GET['myPostcode']
+        extraPrice = request.GET['extraPrice']
+        rcvName = request.GET['rcvName']
+        rcvPhone = request.GET['rcvPhone']
+        rcvAddress = request.GET['rcvAddress']
+        rcvPostcode = request.GET['rcvPostcode']
+        goods = request.GET['goods']
+        expressCompany = request.GET['expressCompany']
+        remarks = request.GET['remarks']
+    # 2.create random string with 10 characters
+    code = (''.join(map(lambda xx: (hex(ord(xx))[2:]), os.urandom(16))))[0:16][0:10]
+    # 3.create Qrcode
     # Todo:optimize the code structure
     q = qrcode.main.QRCode()
-    q.add_data(name+'\n'+address+'\n'+phone)
+    q.add_data(rcvName+'\n'+rcvPhone+'\n'+code)
     q.make()
     m = q.make_image()
-    # 3.create random string with 10 characters
-    code = (''.join(map(lambda xx:(hex(ord(xx))[2:]),os.urandom(16))))[0:16][0:10]
-    # note:qrcode picture's name is code
     # 4.produce the picture
-    m.save(code+'.png')
+    m.save(code+'.png')  #qrcode picture's name is 'code'
     # 5.move it to the pointed directory
     shutil.move(code+'.png','static/polls')
     # 4.create response
