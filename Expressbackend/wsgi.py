7127df74ae0207b32754af2ac67e1dae0611947a
ryh <1971990184@qq.com>
2016-05-09 13:31:15 +0800

    fix API 1

--- Expressbackend/wsgi.py
+++ Expressbackend/wsgi.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Expressbackend.settings")
+
 from os.path import join,dirname,abspath
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
-import sys 
-sys.path.insert(0,PROJECT_DIR) 
-from django.core.wsgi import get_wsgi_application
 
+import sys
+sys.path.insert(0,PROJECT_DIR)
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
