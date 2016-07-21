d20917327906ad6175a835b806449557e90609dc
ryh <1971990184@qq.com>
2016-07-21 13:53:19 +0800

    seperate test module,create shortestPath module

---
+++ Express/shortestPath/findShortest.py
@@ -0,0 +1,12 @@
+# coding:utf8
+
+'''
+find shortest path between two cities
+e.g.
+input:成都 上海
+output：
+[] 中间经过的城市
+[] 每个城市的转移时间
+'''
+def getPath(_from,_to):
+    pass
--- Express/test/testRSA.py
+++ Express/test/testRSA.py
@@ -5,9 +5,10 @@ from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 
-# message = '{"name":"任远航","age":"20","phone":"18161251991"}'
-# message = 'hello ghost, this is a plian text'
-message = '4410a17354'
+# message = {"code" :'6e484a244f',"rcvPhone":'18349342287'}
+# message = json.dumps(message)
+# message = '009'
+message = '007'
 # dictMessage = json.loads(message)
 # print dictMessage
 with open('public.pem') as f:
