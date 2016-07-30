931736b5f59f1c7c65584f95174ee49fdc4aa28d
ryh <1971990184@qq.com>
2016-07-30 12:24:43 +0800

    fix AES encrypt module

--- Express/AES/testAES.py
+++ Express/AES/testAES.py
@@ -1,7 +1,14 @@
+# coding:utf8
 import base64
 from Crypto.Cipher import AES
 # should be 16 bytes long
-obj = AES.new('helloworld123456',AES.MODE_CBC,'helloworld123456')
-message = "The answer is no"
-ciphertext = base64.b64encode(obj.encrypt(message))
-print ciphertext
+from Express.utils.encrypt import encrypt, decrypt
+
+key = '12345678abcdefgh'
+cipher = AES.new(key)
+message = "你好2!"
+
+if __name__ == '__main__':
+    print "Message:",message
+    print "Encrypted:",encrypt(message,cipher)
+    print "Decrypted:",decrypt(encrypt(message,cipher),cipher)
--- Express/utils/encrypt.py
+++ Express/utils/encrypt.py
@@ -1,13 +1,18 @@
 import base64
 from Crypto.Cipher import AES
 
-def encrypt(key,string):
+def pad(s):
     '''
-    using AES encryption Algorithm
-    :param key: 16 bytes
-    :param string: message to be encrypted
-    :return: base64 encoded ciphertext
+    fill in the string so it can divide by 16
+    :param s: message to be encrypted
+    :return: message to be encrypted(multiple of 16 in length)
     '''
-    obj = AES.new(key,AES.MODE_CBC,key)
-    ciphertext = base64.b64encode(obj.encrypt(string))
-    return ciphertext
+    return s+((16-len(s)%16)*'{')
+def encrypt(s,cipher):
+    '''encrypt message then base64 encode it'''
+    return base64.b64encode(cipher.encrypt(pad(s)))
+def decrypt(s,cipher):
+    '''decrypt message then cut its {'''
+    dec = cipher.decrypt(base64.b64decode(s))
+    l = dec.count('{')
+    return dec[:len(dec)-l]
